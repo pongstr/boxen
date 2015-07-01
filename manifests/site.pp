2df971a690531a907560db468d0b61978c254611
Pongstr <twiz.tickler@gmail.com>
2015-07-01 16:09:54 +0800

    added mongodb + robomongo

--- manifests/site.pp
+++ manifests/site.pp
@@ -55,7 +55,8 @@ node default {
   # core modules, needed for most things
   include git
   include hub
-
+  include brewcask
+  
   # fail if FDE is not enabled
   if $::root_encrypted == 'no' {
     fail('Please enable full disk encryption and try again')
