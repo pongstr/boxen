09967e335149c8bdd538780e9933a356fa96a1d5
Pongstr <twiz.tickler@gmail.com>
2015-07-01 21:55:12 +0800

    Added Default Node Modules and MySql
    - updated Mjolnir group with default node modules
    - added mysql, totally irrelevant to this commit batch

--- modules/groups/manifests/mjolnir.pp
+++ modules/groups/manifests/mjolnir.pp
@@ -7,9 +7,74 @@ class groups::mjolnir {
   include dashlane
   include firefox
   include hipchat
+  include mongodb
   include sublime_text
 
-  package { 'robomongo': provider => 'brewcask' }
-  package { 'github': provider => 'brewcask' }
+  # Node.js Global Version
+  class { 'nodejs::global':
+    version => 'v0.12.5'
+  }
+
+  # Global Node Modules
+  nodejs::module { 'bower':
+    node_version => 'v0.12.5'
+  }
+
+  nodejs::module { 'coffee-script':
+    node_version => 'v0.12.5'
+  }
+
+  nodejs::module { 'express':
+    node_version => 'v0.12.5'
+  }
+
+  nodejs::module { 'grunt-cli':
+    node_version => 'v0.12.5'
+  }
+
+  nodejs::module { 'gulp':
+    node_version => 'v0.12.5'
+  }
+
+  nodejs::module { 'jshint':
+    node_version => 'v0.12.5'
+  }
+
+  nodejs::module { 'karma-cli':
+    node_version => 'v0.12.5'
+  }
+
+  nodejs::module { 'less':
+    node_version => 'v0.12.5'
+  }
+
+  nodejs::module { 'strongloop':
+    node_version => 'v0.12.5'
+  }
+
+  nodejs::module { 'mean-cli':
+    node_version => 'v0.12.5'
+  }
+
+  nodejs::module { 'nodemon':
+    node_version => 'v0.12.5'
+  }
+
+  nodejs::module { 'pm2':
+    node_version => 'v0.12.5'
+  }
+
+  nodejs::module { 'shelljs':
+    node_version => 'v0.12.5'
+  }
+
+  package { 'robomongo':
+    provider => 'brewcask',
+    ensure   => 'installed'
+  }
+
+  # buggy at the moment, throws
+  # Error: Execution of 'brew cask install github' returned 1
+  # package { 'github': provider => 'brewcask' }
 
 }
