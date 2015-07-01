5c8ee48547f0f52113df19a7190162badca5a2e3
Pongstr <twiz.tickler@gmail.com>
2015-07-01 23:08:26 +0800

    Added puppet-osx and personal dotfile settings
    this commit is basically a demo on how you can customize your
    preferences, symlinking dotfiles makes things clean. i think

--- modules/groups/manifests/mjolnir.pp
+++ modules/groups/manifests/mjolnir.pp
@@ -4,11 +4,16 @@ class groups::mjolnir {
   notify { 'Hello Mjolnir member, we\'re setting up your shit, sit back and relax.': }
 
   include brewcask
+  include chrome
+  include chrome::canary
+  include chrome::chromium
   include dashlane
   include firefox
   include hipchat
   include mongodb
+  # include osx
   include sublime_text
+  include zsh
 
   # Node.js Global Version
   class { 'nodejs::global':
--- modules/people/manifests/pongstr.pp
+++ modules/people/manifests/pongstr.pp
@@ -1,6 +1,45 @@
 
 class people::pongstr {
+
   require groups::mjolnir
-  notify { "Hello ${::boxen_user}": }
+
+  notify { "Hello ${::boxen_user}, Let's get you bootstrapped.": }
+
+  $home     = "/Users/${::boxen_user}"
+  $projects = "${home}/Projects"
+  $dotfiles = "${home}/dotfiles"
+
+  # Initialize Dotfiles Location
+  file { $dotfiles:
+    ensure => directory
+  }
+
+  # Fetch Dotfiles from Repository
+  repository { $dotfiles:
+    source  => 'pongstr/dotfiles',
+    require => File[$dotfiles]
+  }
+
+  # Create Vim Directory for all vim related stuff
+  exec { 'Create .vim directory':
+    command     => "mkdir -p ${home}/.vim/.viminfo ${home}/.vim/backups ${home}/.vim/colors ${home}/.vim/swaps ${home}/.vim/undo",
+    refreshonly => false
+  }
+
+  # Link .vimrc from dotfile
+  file { $dotfiles:
+    ensure  => link,
+    mode    => '0644',
+    target  => "{$home}/.vimrc",
+    require => File["${dotfiles}/bin/vim/.vimrc"]
+  }
+
+  # Link Pongstr Base-16 theme
+  file { $dotfiles:
+    ensure  => link,
+    mode    => '0644',
+    target  => "${home}/.vim/colors/Pongstr Base-16.vim",
+    require => File["${dotfiles}/bin/vim/Pongstr Base-16.vim"],
+  }
 
 }
