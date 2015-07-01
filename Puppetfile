8409a5438561875d76eb7f2c5214328f6cd0044a
Pongstr <twiz.tickler@gmail.com>
2015-07-01 15:27:55 +0800

    tmp

--- Puppetfile
+++ Puppetfile
@@ -68,3 +68,4 @@ github "xquartz",     "1.2.1"
 
 github "firefox",     "1.2.3"
 github "hipchat",     "1.1.3"
+github "dashlane",    "1.0.1"
--- modules/groups/manifests/mjolnir.pp
+++ modules/groups/manifests/mjolnir.pp
@@ -3,6 +3,7 @@
 class groups::mjolnir {
   notify { 'Hello Mjolnir member, we\'re setting up your shit, sit back and relax.': }
 
+  include dashlane
   include firefox
   include hipchat
 
