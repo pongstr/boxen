85897aa07dde7a4024d0a7dafe5df9878922bfca
Pongstr <twiz.tickler@gmail.com>
2015-07-02 12:46:19 +0800

    Squashed commit of the following:
    commit 446304355614888be691d155001f608de420acba
    Author: Pongstr <twiz.tickler@gmail.com>
    Date:   Thu Jul 2 12:45:24 2015 +0800
    
        bugfixes
    
    commit 26bed50494a2b2ad2e49178decef8b097b1c3140
    Author: Pongstr <twiz.tickler@gmail.com>
    Date:   Thu Jul 2 12:42:10 2015 +0800
    
        bugfixes
    
    commit 0a3659de149af73bc1095dd75063a3be5cfeefbe
    Author: Pongstr <twiz.tickler@gmail.com>
    Date:   Thu Jul 2 12:30:34 2015 +0800
    
        Fix for people::pongstr
    
        - fix repository syntax, clone to `.dotfiles` directory

--- modules/people/manifests/pongstr.pp
+++ modules/people/manifests/pongstr.pp
@@ -4,12 +4,17 @@ class people::pongstr {
   # Front-end Developer Classification
   require groups::mjolnir
 
+  package { 'vim':
+    ensure          => present,
+    install_options => ['--override-system-vi']
+  }
+
   notify { "Hello ${::boxen_user}, Let's get you bootstrapped.": }
   notify { "We're now going to run the setup to make you feel at home.": }
 
   $home     = "/Users/${::boxen_user}"
   $projects = "${home}/Projects"
-  $dotfiles = "${home}/dotfiles"
+  $dotfiles = "${home}/.dotfiles"
   $ohmyzsh  = "${home}/.oh-my-zsh"
 
   # Initialize Dotfiles Location
@@ -19,9 +24,10 @@ class people::pongstr {
 
   # Fetch Dotfiles from Repository
   repository { $dotfiles:
-    ensure  => 'origin/boxen',
-    source  => 'pongstr/dotfiles',
-    require => File[$dotfiles],
+    path     => $dotfiles,
+    ensure   => 'origin/boxen',
+    source   => 'pongstr/dotfiles',
+    provider => 'git',
   }
 
   # Create Vim Directory for all vim related stuff
