73da1f6226e7c3e517c92250c3abd189f700ea6e
femee24 <fhem24_elle@yahoo.com>
2015-07-02 11:19:09 +0800

    added vagrant and boot2docker and mysql on the osiris

--- modules/groups/manifests/osiris.pp
+++ modules/groups/manifests/osiris.pp
@@ -1,13 +1,23 @@
-# Osiris: Installation Setup for System Administrators
+ # Osiris: Installation Setup for System Administrators
 
 class groups::osiris {
- notify { 'Hello Osiris Member, we\'re setting up your workstations': }
+  notify { 'Hello Osiris Member, we\'re setting up your workstations': }
 
   include chrome
-  include chrome::canary
-  include atom
+  include firefox
+  include hipchat
+  include dashlane
+  include sublime_text
+  #include atom
   include vlc
   include virtualbox
   include iterm2::stable
+  include mysql
+      mysql::db { 'database': }
 
+      vagrant::box { 'squeeze64/vmware_fusion':
+        source => 'https://s3.amazonaws.com/github-ops/vagrant/squeeze64-6.0.7-vmware_fusion.box'
+      }
+
+      package { 'boot2docker': provider => 'homebrew' }
 }
