2e787047ccdb3280e8bd9af693a0e30f69e2e7cf
Reijhanniel Jearl Campos <devcsrj@gmail.com>
2015-07-03 14:51:13 +0800

    [Vishnu] - IDEs configured

--- modules/groups/manifests/vishnu.pp
+++ modules/groups/manifests/vishnu.pp
@@ -3,4 +3,14 @@
 class groups::vishnu {
   notify { 'Hello Vishnu member, Let\'s set you up.': }
 
+  include firefox
+  include chrome
+  include dashlane
+  include hipchat
+  include sublime_text
+
+  include java
+
+  # include eclipse::jee # installable, but fails at `chown`
+  include netbeans
 }
