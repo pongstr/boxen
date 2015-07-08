6ac65a44f8ef8f802919521f18a6beb705791985
Administrator <administrator@Administrators-MacBook-Pro.local>
2015-07-08 15:21:36 +0800

    added ['--appdir=/Applications'] for brew cask

--- modules/people/manifests/femee24.pp
+++ modules/people/manifests/femee24.pp
@@ -1,4 +1,10 @@
 class people::femee24 {
   require groups::osiris
   notify { 'Hello ${::boxen_user}': }
+
+  package { 'adobe-creative-cloud':
+    ensure          => installed,
+    provider        => 'brewcask',
+    install_options => ['--no-binaries'],
+  }
 }
