f39679c977137b7bdf1b305c432397426a490347
Reijhanniel Jearl Campos <devcsrj@gmail.com>
2015-07-20 12:18:04 +0800

    People/Puppets - Backend +1 (djtoro)

---
+++ modules/people/manifests/djtoro.pp
@@ -0,0 +1,10 @@
+
+class people::djtoro {
+
+  require groups::shiva
+
+  notify { "Hello ${::boxen_user}, Let's get you bootstrapped.": }
+  notify { "We're now going to run the setup to make you feel at home.": }
+
+
+}
