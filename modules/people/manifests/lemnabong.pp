3fca61a0b833fde1df7470028f9ac9f0b6c47a65
toroboxen <femee.canlas@gmail.com>
2015-07-22 14:28:10 +0800

    spectacle added to lemnabong

--- modules/people/manifests/lemnabong.pp
+++ modules/people/manifests/lemnabong.pp
@@ -7,5 +7,9 @@ class people::lemnabong {
   notify { "Hello ${::boxen_user}, Let's get you bootstrapped.": }
   notify { "We're now going to run the setup to make you feel at home.": }
 
-
+  package { 'spectacle':
+    ensure   => installed,
+    provider => 'compressed_app',
+    source   => 'https://s3.amazonaws.com/spectacle/downloads/Spectacle+0.8.10.zip'
+  }
 }
