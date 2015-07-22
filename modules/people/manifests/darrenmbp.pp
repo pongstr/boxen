6fecc1e875dde75408bd5b46379eba2547bb0e13
toroboxen <femee.canlas@gmail.com>
2015-07-22 08:05:20 +0800

    added darrenmbp & added IntelliJ and JAVA6

---
+++ modules/people/manifests/darrenmbp.pp
@@ -0,0 +1,24 @@
+
+class people::artastillero {
+
+  # Back-end Developer Classification
+  require groups::vishnu
+
+  include tunnelblick
+
+  notify { "Hello ${::boxen_user}, Let's get you bootstrapped.": }
+  notify { "We're now going to run the setup to make you feel at home.": }
+
+
+package { 'intellij-idea':
+  ensure          => installed,
+  provider        => 'brewcask',
+  install_options => ['--no-binaries','--appdir=/Applications'],
+}
+
+package { 'java6':
+  ensure          => installed,
+  provider        => 'brewcask',
+}
+
+}
