19020d399a9720ca69911115d1ce18552188e366
Reijhanniel Jearl Campos <rj.campos@toro.io>
2015-07-23 15:00:25 +0800

    Added xampp for cturla

--- modules/people/manifests/cturla.pp
+++ modules/people/manifests/cturla.pp
@@ -6,6 +6,11 @@ class people::cturla {
 
   notify { "Hello ${::boxen_user}, Let's get you bootstrapped.": }
   notify { "We're now going to run the setup to make you feel at home.": }
-
+  
+  package { 'xampp':
+    ensure          => installed,
+    provider        => 'brewcask',
+    install_options => ['--no-binaries','--appdir=/Applications'],
+  }
 
 }
