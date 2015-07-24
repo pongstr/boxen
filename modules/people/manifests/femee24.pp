d3b0e49f8fc6d0ffbcdeb97832150f7966d5f9fc
toroboxen <femee.canlas@gmail.com>
2015-07-24 10:12:50 +0800

    Added manifest for impakt

--- modules/people/manifests/femee24.pp
+++ modules/people/manifests/femee24.pp
@@ -1,4 +1,5 @@
 class people::femee24 {
+  
   require groups::osiris
   notify { 'Hello ${::boxen_user}': }
 
---
+++ modules/people/manifests/impakt.pp
@@ -0,0 +1,43 @@
+
+class people::impakt {
+
+  # Applications for Daren Klamer Only
+
+  include sublime_text
+  include textwrangler
+  include virtualbox
+
+  notify { "Hello ${::boxen_user}, Let's get you bootstrapped.": }
+  notify { "We're now going to run the setup to make you feel at home.": }
+
+  package { 'intellij-idea':
+    ensure          => installed,
+    provider        => 'brewcask',
+    install_options => ['--no-binaries','--appdir=/Applications'],
+  }
+
+  package { 'java':
+    ensure          => installed,
+    provider        => 'brewcask',
+  }
+
+  package { 'filezilla':
+    ensure          => installed,
+    provider        => 'brewcask',
+    install_options => ['--no-binaries','--appdir=/Applications'],
+  }
+
+  package { 'skype':
+    ensure          => installed,
+    provider        => 'brewcask',
+    install_options => ['--no-binaries','--appdir=/Applications'],
+  }
+
+  package { 'remote-desktop-manager':
+    ensure          => installed,
+    provider        => 'brewcask',
+    install_options => ['--no-binaries','--appdir=/Applications'],
+  }
+
+
+}
