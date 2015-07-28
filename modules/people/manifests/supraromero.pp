24ba082573029c9474382c5c0e14b24bfd09cf22
Reijhanniel Jearl Campos <rj.campos@toro.io>
2015-07-28 10:10:10 +0800

    #135 : Added displaylink-driver for People/supra.pp

--- modules/people/manifests/supraromero.pp
+++ modules/people/manifests/supraromero.pp
@@ -7,5 +7,10 @@ class people::supraromero {
   notify { "Hello ${::boxen_user}, Let's get you bootstrapped.": }
   notify { "We're now going to run the setup to make you feel at home.": }
 
-
+  package { 'displaylink-driver':
+    ensure   => installed,
+    provider => 'pkgdmg',
+    source   => 'http://192.168.21.151/DisplayLink_OSX_2.4.dmg'
+  }
+  
 }
