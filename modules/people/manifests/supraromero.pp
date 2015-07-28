796acc2ce59f458c800c849c5dfd8941efa600e7
Pongstr <twiz.tickler@gmail.com>
2015-07-29 06:05:57 +0800

    added class::drivers

--- modules/people/manifests/supraromero.pp
+++ modules/people/manifests/supraromero.pp
@@ -3,15 +3,10 @@ class people::supraromero {
 
   # Back-end Developer Classification
   require groups::shiva
+  require drivers:displaylink
 
   notify { "Hello ${::boxen_user}, Let's get you bootstrapped.": }
   notify { "We're now going to run the setup to make you feel at home.": }
-
-  package { 'displaylink-driver':
-    ensure   => installed,
-    provider => 'pkgdmg',
-    source   => 'http://192.168.21.151/DisplayLink_OSX_2.4.dmg'
-  }
 
   package { 'remote-desktop-manager':
     ensure          => installed,
