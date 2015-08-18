5c138fd9a6016a68067b6f28717c78c6f610d473
Reijhanniel Jearl Campos <rj.campos@toro.io>
2015-08-18 11:47:57 +0800

    #192 - Added xampp to people/cjsn1221

--- modules/people/manifests/cjsn1221.pp
+++ modules/people/manifests/cjsn1221.pp
@@ -7,5 +7,10 @@ class people::cjsn1221 {
   notify { "Hello ${::boxen_user}, Let's get you bootstrapped.": }
   notify { "We're now going to run the setup to make you feel at home.": }
 
+  package { 'xampp':
+    ensure          => installed,
+    provider        => 'brewcask',
+    install_options => ['--no-binaries','--appdir=/Applications'],
+  }
 
 }
