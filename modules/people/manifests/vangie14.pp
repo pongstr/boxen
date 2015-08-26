d0ca12c9f55757535099e1f90f29e17368c214db
Pongstr <twiz.tickler@gmail.com>
2015-08-27 06:03:13 +0800

    fix unecessary re-declarations

--- modules/people/manifests/vangie14.pp
+++ modules/people/manifests/vangie14.pp
@@ -3,23 +3,4 @@ class people::vangie14 {
   require groups::ganesha
   notify { 'Hello ${::boxen_user}': }
 
-
-  package { 'remote-desktop-manager':
-    ensure          => installed,
-    provider        => 'brewcask',
-    install_options => [
-      '--no-binaries',
-      '--appdir=/Applications'
-    ]
-  }
-
-  package { 'skype':
-    ensure          => installed,
-    provider        => 'brewcask',
-    install_options => [
-      '--no-binaries',
-      '--appdir=/Applications'
-    ]
-  }
-
 }
