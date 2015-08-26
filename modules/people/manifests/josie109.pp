c5974a97a31b498834a591136c6875112dbff6df
femee24 <fhem24_elle@yahoo.com>
2015-08-26 13:39:35 +0800

    Admin account added

---
+++ modules/people/manifests/josie109.pp
@@ -0,0 +1,25 @@
+class people::josie109 {
+
+  require groups::ganesha
+  notify { 'Hello ${::boxen_user}': }
+
+
+  package { 'remote-desktop-manager':
+    ensure          => installed,
+    provider        => 'brewcask',
+    install_options => [
+      '--no-binaries',
+      '--appdir=/Applications'
+    ]
+  }
+
+  package { 'skype':
+    ensure          => installed,
+    provider        => 'brewcask',
+    install_options => [
+      '--no-binaries',
+      '--appdir=/Applications'
+    ]
+  }
+
+}
