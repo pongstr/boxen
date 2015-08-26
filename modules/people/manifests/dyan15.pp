acd6c37564133cde8880d257c953bb7dc0110e63
femee24 <fhem24_elle@yahoo.com>
2015-08-26 14:19:34 +0800

    Added cord for mam josie and mam dyan

--- modules/people/manifests/dyan15.pp
+++ modules/people/manifests/dyan15.pp
@@ -3,7 +3,15 @@ class people::dyan15 {
   require groups::ganesha
   notify { 'Hello ${::boxen_user}': }
 
-
+  package { 'cord':
+    ensure          => installed,
+    provider        => 'brewcask',
+    install_options => [
+      '--no-binaries',
+      '--appdir=/Applications'
+    ]
+  }
+  
   package { 'remote-desktop-manager':
     ensure          => installed,
     provider        => 'brewcask',
