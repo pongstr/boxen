1a0f1e1e604ff9edb0802c11f625e53c52c30efb
Reijhanniel Jearl Campos <rj.campos@toro.io>
2015-09-01 10:39:26 +0800

    Added mysql to people/itsvicsoto

--- modules/people/manifests/itsvicsoto.pp
+++ modules/people/manifests/itsvicsoto.pp
@@ -5,6 +5,7 @@ class people::itsvicsoto {
   require groups::mjolnir
 
   include tunnelblick
+  include mysql
 
   notify { "Hello ${::boxen_user}, Let's get you bootstrapped.": }
   notify { "We're now going to run the setup to make you feel at home.": }
