b693e7505c940ad639a8912c73bf071e1ff3b471
femee24 <fhem24_elle@yahoo.com>
2015-10-02 11:02:59 +0800

    Remove groups/mjolnir

--- modules/people/manifests/jeff322.pp
+++
@@ -1,5 +0,0 @@
-class people::jeff322 {
-  require groups::osiris
-  notify { 'Hello ${::boxen_user}': }
-
-}
--- modules/people/manifests/supraromero.pp
+++ modules/people/manifests/supraromero.pp
@@ -2,7 +2,7 @@ class people::supraromero {
 
   # Back-end Developer Classification
   require groups::vishnu
-  #require drivers:displaylink
+  require drivers:displaylink
 
   notify { "Hello ${::boxen_user}, Let's get you bootstrapped.": }
   notify { "We're now going to run the setup to make you feel at home.": }
